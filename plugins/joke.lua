b298499663758f55fa88dbaa37a7b550b2d8e48a
ArashEmp <ArashEmp@users.noreply.github.com>
2016-03-29 19:36:03 -0700

    Update joke.lua

--- plugins/joke.lua
+++ plugins/joke.lua
@@ -1,1 +1,11 @@
+local  = 'joke_data.lua'
+local function run(msg)
+return joke[math.random(#joke)]
+end
 
+return {
+description = "500 Persian Joke",
+usage = "!joke : send random joke",
+patterns = {"^[Jj]oke$"},
+run = run
+}
