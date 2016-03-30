5786bda32eae48681806a61567a7c8184da9518c
Shahab <safaeishahab@yahoo.com>
2016-03-31 01:24:57 +0430

    Create Info.lua

---
+++ plugins/Info.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return   "نام کامل:  "..(msg.from.print_name or '').."\nنام کوچک: "..(msg.from.first_name or '').."\nنام خانوادگی:  "..(msg.from.last_name or '').."\n\nشماره موبایل: +"..(msg.from.phone or '').."\nیوزرنیم: @"..(msg.from.username or '').."\nآی دی: "..msg.from.id.."\nرابط کاربری: موبایل\nنام گروه: "..msg.to.title.."\nآی دی گروه: 
+"..msg.to.id.."
+end
+return {
+  description = "", 
+  usage = "",
+  patterns = {
+    "^[Ii]nfo$",
+  },
+  run = run
+}
+end
