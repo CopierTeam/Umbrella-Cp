2051ed7321fa077286636a5c045dbde3dec0d4b4
AmirSbss <mrahs@emeil.ir>
2016-03-30 19:18:53 +0430

    Update Web_Shot.lua

--- plugins/Web_Shot.lua
+++ plugins/Web_Shot.lua
@@ -1,56 +1,20 @@
-local helpers = require "OAuth.helpers"
-
-local base = 'https://screenshotmachine.com/'
-local url = base .. 'processor.php'
-
-local function get_webshot_url(param)
-   local response_body = {}
-   local request_constructor = {
-      url = url,
-      method = "GET",
-      sink = ltn12.sink.table(response_body),
-      headers = {
-         referer = base,
-         dnt = "1",
-         origin = base,
-         ["User-Agent"] = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"
-      },
-      redirect = false
-   }
-
-   local arguments = {
-      urlparam = param,
-      size = "FULL"
-   }
-
-   request_constructor.url = url .. "?" .. helpers.url_encode_arguments(arguments)
-
-   local ok, response_code, response_headers, response_status_line = https.request(request_constructor)
-   if not ok or response_code ~= 200 then
-      return nil
-   end
-
-   local response = table.concat(response_body)
-   return string.match(response, "href='(.-)'")
-end
+do
 
 local function run(msg, matches)
-   local find = get_webshot_url(matches[1])
-   if find then
-      local imgurl = base .. find
-      local receiver = get_receiver(msg)
-      send_photo_from_url(receiver, imgurl)
-   end
+  local eq = URL.escape(matches[1])
+
+  local url = "http://api.screenshotmachine.com/?key=b645b8&size=NMOB&url="..eq
+
+  local receiver = get_receiver(msg)
+  send_sticker_from_url(receiver, url, send_title, {receiver, title})
 end
 
-
 return {
-   description = "Send an screenshot of a website.",
-   usage = {
-      "web [url]: Take an screenshot of the web and send it back to you."
-   },
-   patterns = {
-      "^[Ww]eb (https?://[%w-_%.%?%.:/%+=&]+)$",
-   },
-   run = run
+  patterns = {
+    "^[Ww]eb (.+)$"
+  },
+  run = run
 }
+
+end
+--plugin by mehrpouya
