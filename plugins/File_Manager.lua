cfb8f034c911cb8f677d0bda79b2f71be228dbe6
AmirSbss <mrahs@emeil.ir>
2016-03-30 11:40:05 +0430

    Added files via upload

---
+++ plugins/File_Manager.lua
@@ -0,0 +1,31 @@
+local function run(msg, matches)
+  if matches[1] == "send" then
+    local file = matches[2]
+    if is_sudo(msg) then
+      local receiver = get_receiver(msg)
+      send_document(receiver, "./plugins/"..file..".lua", ok_cb, false)
+    end
+  end
+end
+if matches[1] == 'addplug' and is_sudo(msg) then
+  local text = matches[2]
+  local b = 1
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  local name = matches[3]
+  local file = io.open("./plugins/"..name..matches[4], "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  return "done"
+  end
+  return {
+  patterns = {
+  "^([Ss]end) (.*)$",
+  "^([Aa]ddplug)(.+) (.*) (.*)$"
+  },
+  run = run
+}
+end
