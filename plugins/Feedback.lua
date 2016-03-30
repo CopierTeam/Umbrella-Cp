7b842db8079b7d76bd605640951a9bf7e90472d4
ArashEmp <ArashEmp@users.noreply.github.com>
2016-03-30 15:53:28 -0700

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -1,7 +1,7 @@
 do
  function run(msg, matches)
  
- local fuse = 'New FeedBack\nUser#ID Just We Received From User "..(msg.from.username or msg.from.id).." : ' .. msg.from.id .. '\nName : ' .. msg.from.print_name ..'\nUsername : @' .. msg.from.username .. '\n\nLocal Message:\n' .. matches[1] 
+ local fuse = 'New FeedBack Just We Received From User "..(msg.from.username or msg.from.id).."\nUser#ID : ['..msg.from.id..']\nName : ' .. msg.from.print_name ..'\nUsername : @' .. msg.from.username .. '\n\nLocal Message:\n\n' .. matches[1] 
  local fuses = '!printf user#id' .. msg.from.id
  
  
@@ -10,7 +10,7 @@ do
    --like : local chat = "chat#id"..12345678
    
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'Sent!'
+  return 'Thanks For Your Feedback User['..msg.from.id..']'
  
  end
  end
