a4c2468f5512937c16d65f7f5c2c0e0e9ff4095e
AmirSbss <mrahs@emeil.ir>
2016-03-30 11:25:39 +0430

    Update Block.lua

--- plugins/Block.lua
+++ plugins/Block.lua
@@ -1,27 +1,18 @@
-do
 
 local function run(msg, matches)
-if is_admin(msg) then
- local user = matches[2]
-  if matches[1] == "+" then
-      user = 'user#id'..user
-      block_user(user, ok_cb, false)
-    return "یوزر ["..matches[2].."] بلاک شد"
-  end
-if matches[1] == "-" then
-user = "user#id"..matches[2]
-unblock_user(user, ok_cb, false)
-return "یوزر ["..matches[2].."] انبلاک شد"
-end
-end
-end
-
+    if matches[1]:lower() == 'block' and is_sudo(msg) then
+        block_user("user#id"..matches[2],ok_cb,false)
+        return "User blocked"
+    end
+    if matches[1]:lower() == 'unblock' and is_sudo(msg) then
+	    unblock_user("user#id"..matches[2],ok_cb,false)
+        return "User unblocked"
+    end
 return {
-description = "بلاک یا انبلاک کردن از پی وی",
-usage = "Block [+/-] [User-ID] : بلاک یا انبلاک کردن از پی وی",
   patterns = {
-    "^[Bb]lock (+) (%d+)$",
-    "^[Bb]lock (-) (%d+)$",
+	"^([Uu]nblock) (%d+)$",
+	"^([Bb]lock) (%d+)$"
   },
   run = run,
 }
+end
