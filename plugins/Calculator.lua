6fc6cb5f78acf75b0eb4627d0f7d075cea15a095
ArashEmp <ArashEmp@users.noreply.github.com>
2016-03-29 19:50:43 -0700

    Create Calculator.lua

--- plugins/Calculator.lua
+++ plugins/Calculator.lua
@@ -6,7 +6,7 @@ local function mathjs(exp)
   local b,c = http.request(url)
   local text = nil
   if c == 200 then
-    text = 'Result: '..b
+    text = ''..b
   
   elseif c == 400 then
     text = b
@@ -23,9 +23,9 @@ end
 
 return {
   description = "Calculate math expressions with mathjs API",
-  usage = "!calc [expression]: evaluates the expression and sends the result.",
+  usage = "Calc [expression]: evaluates the expression and sends the result.",
   patterns = {
-    "^!calc (.*)$"
+    "^[Cc]alc (.*)$"
   },
   run = run
 }
