8fdf147530cdc56b76430b273eea1351b5f68f78
Shahab <safaeishahab@yahoo.com>
2016-03-31 14:14:19 +0430

    Update Feedback.lua
    Send feedback to pv :^)

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -6,8 +6,8 @@ do
  
  
    local text = matches[1]
-   local chat = "chat#id"..YourChatId 
-   --like : local chat = "chat#id"..12345678
+   local user = "user#id"..YourChatId 
+   --like : local user = "user#id"..12345678
    
   local sends = send_msg(chat, fuse, ok_cb, false)
   return 'Thanks For Your Feedback User['..msg.from.id..']'
