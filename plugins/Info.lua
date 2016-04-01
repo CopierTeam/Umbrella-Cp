52fd284cad8a85d2a1f178edb52d78ee13ab562e
AmirSbss <mrahs@emeil.ir>
2016-04-01 15:39:01 +0430

    Create Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
   return   "نام کامل:  "..(msg.from.print_name or '').."\nنام کوچک: "..(msg.from.first_name or '').."\nنام خانوادگی:  "..(msg.from.last_name or '').."\n\nشماره موبایل: +"..(msg.from.phone or '').."\nیوزرنیم: @"..(msg.from.username or '').."\nآی دی: "..msg.from.id.."\nرابط کاربری: موبایل\nنام گروه: "..msg.to.title.."\nآی دی گروه: 
-"..msg.to.id.."
+"..msg.to.id.."\n#Cper"
 end
 return {
   description = "", 
@@ -14,3 +14,4 @@ return {
 }
 end
 --by Shahabsaf @ShahabHiDDeN
+--will be update soon!
