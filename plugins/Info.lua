a7b0b76d2bf1a0513dc3ac42664d7a500dc8a861
AmirSbss <mrahs@emeil.ir>
2016-05-10 16:26:47 +0430

    Debuged!

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -200,6 +200,7 @@ local function callback_res(extra, success, result)
 	else
 		userrank = "Member"
 	end
+	end
 	--custom rank ------------------------------------------------------------------------------------------------
 	local file = io.open("./info/"..result.id..".txt", "r")
 	if file ~= nil then
@@ -298,6 +299,7 @@ local function callback_info(extra, success, result)
 			send_document(org_chat_id,"umbrella/stickers/api.webp", ok_cb, false)
 	else
 		userrank = "Member"
+	end
 	end
 	--custom rank ------------------------------------------------------------------------------------------------
 	local file = io.open("./info/"..result.id..".txt", "r")
