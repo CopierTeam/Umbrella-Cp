578af8910f49e2f59f27c915e176cb0b77a05f37
AmirSbss <mrahs@emeil.ir>
2016-05-13 23:56:01 +0430

    A Little Update

--- plugins/Logo.lua
+++ plugins/Logo.lua
@@ -1,18 +1,16 @@
-
+do
 local function run(msg, matches)
-if matches[1] == "logo>" then
     local receiver = get_receiver(msg)
-    local site = matches[2]
-	local url = "http://logo.clearbit.com/"..site.."?size=500&greyscale=true"
+    local site = matches[2]:lower()
+if matches[1]:lower() == "logo>" then
+	local url = "http://logo.clearbit.com/"..site.."?size=800&greyscale=true"
 	local randoms = math.random(1000,900000)
 	local randomd = randoms..".jpg"
 	local file = download_to_file(url,randomd)
 	local cb_extra = {file_path=file}
     send_photo(receiver, file, rmtmp_cb, cb_extra)
 end
-if matches[1] == "logo" then
-local receiver = get_receiver(msg)
-    local site = matches[2]
+if matches[1]:lower() == "logo" then
 	local url = "http://logo.clearbit.com/"..site.."?size=800"
 	local randoms = math.random(1000,900000)
 	local randomd = randoms..".jpg"
@@ -23,10 +21,9 @@ end
 end
 return {
   patterns = {
-		"^[#!/](logo>) (.*)$",
-                "^[#!/](logo) (.*)$",
-                "^(logo>) (.*)$",
-                "^(logo) (.*)$",
+                "^([Ll]ogo>) (.*)$",
+                "^([Ll]ogo) (.*)$",
   }, 
   run = run 
 }
+end
